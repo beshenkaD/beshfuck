89f6a400ebe6c6dd620125d6eebbfc30eb54c184
Beshenka <mambatamba6@gmail.com>
2022-06-11 19:10:45 +0300

    fix bytecode

--- src/vm_bytecode.c
+++ src/vm_bytecode.c
@@ -1,15 +1,18 @@
 #include "vm_bytecode.h"
 #include <stdlib.h>
 
+#define INITIAL_CAPACITY 32
+
 void vm_bytecode_init(Bytecode *bc)
 {
-	bc->code = NULL;
-	bc->count = bc->capacity = 0;
+	bc->code = malloc(INITIAL_CAPACITY * sizeof(uint8_t));
+	bc->count = 0;
+	bc->capacity = INITIAL_CAPACITY;
 }
 
 void vm_bytecode_push(Bytecode *bc, uint8_t opcode)
 {
-	if (bc->capacity < bc->count + 1) {
+	if (bc->capacity == bc->count) {
 		bc->capacity *= 2;
 		bc->code = realloc(bc->code, bc->capacity * sizeof(uint8_t));
 	}
@@ -20,5 +23,6 @@ void vm_bytecode_push(Bytecode *bc, uint8_t opcode)
 void vm_bytecode_free(Bytecode *bc)
 {
 	free(bc->code);
-	vm_bytecode_init(bc);
+	bc->code = NULL;
+	bc->count = bc->capacity = 0;
 }
