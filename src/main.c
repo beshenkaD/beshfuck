2ed0e68c3d65b7cc02ef46b7456ba80a857f9204
Beshenka <mambatamba6@gmail.com>
2022-06-11 19:12:25 +0300

    add simple interpreter and examples

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
+#include "compiler.h"
+#include "vm.h"
+#include "vm_bytecode.h"
+#include <assert.h>
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	assert(argc >= 2);
+
+	FILE *src = fopen(argv[1], "rb");
+	assert(src);
+
+	Bytecode bc = compiler_do_file(src);
+
+	Vm *vm = vm_new(1000 * 30, &bc);
+
+	vm_do(vm);
+
+	fclose(src);
+	vm_bytecode_free(&bc);
+	vm_free(vm);
+
 	return 0;
 }
